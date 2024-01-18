d5810cefc1a12032db7cb8af8d292793e97729ef
Sylvain LE GAL <sylvain.legal@grap.coop>
2024-01-18 21:56:59 +0100

    [IMP] pos_product_sort: pre-commit stuff

--- pos_product_sort/__manifest__.py
+++ pos_product_sort/__manifest__.py
@@ -3,21 +3,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Point of Sale - Products Sorted by Name',
-    'version': '12.0.1.0.2',
-    'category': 'Point Of Sale',
-    'summary': '''
+    "name": "Point of Sale - Products Sorted by Name",
+    "version": "12.0.1.0.2",
+    "category": "Point Of Sale",
+    "summary": """
         sort the products by name in the point of sale
         instead of sorting them by the sequence field.
-        ''',
-    'author': 'GRAP, Odoo Community Association (OCA)',
-    'maintainers': ['legalsylvain'],
-    'website': 'https://github.com/OCA/pos',
-    'license': 'AGPL-3',
-    'depends': [
-        'point_of_sale',
+        """,
+    "author": "GRAP, Odoo Community Association (OCA)",
+    "maintainers": ["legalsylvain"],
+    "website": "https://github.com/OCA/pos",
+    "license": "AGPL-3",
+    "depends": [
+        "point_of_sale",
     ],
-    'data': [
-        'views/assets.xml',
+    "data": [
+        "views/assets.xml",
     ],
 }
